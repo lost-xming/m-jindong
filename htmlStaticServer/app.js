065a75b9e31180638f0be1bf1f1a7e8e57f1ae38
wangwenlin <wangwenlin@innotechx.com>
2020-11-05 20:27:57 +0800

    m站

--- htmlStaticServer/app.js
+++ htmlStaticServer/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("/wwl-m-jinDong/dist/"));
+app.use(express.static("/wwl-m-jinDong/build/"));
 app.get("/", function (req, res) {
 	res.send("Hello World!");
 });
